6e84b80c737e238fa79676176a29b003f0fede1f
Edwin Cheng <edwin0cheng@gmail.com>
2019-02-13 03:28:51 +0800

    Change file structure to use upm

---
+++ Editor/PackageInfo.cs
@@ -0,0 +1,13 @@
+namespace ImageEffectGraph.Editor
+{
+    public static class ImageEffectGraphPackageInfo
+    {
+        public static string assetPackagePath
+        {
+            get
+            {
+                return "Packages/com.github.ibicha.image-effect-graph";
+            }
+        }
+    }
+}
